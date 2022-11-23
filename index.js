f3b881a43ac5337bb098b0cd500da0e665fe261a
Mayank-TN <mtandon10@gmail.com>
2022-11-23 15:12:45 +0530

    different url

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log('Mayank Tandon')
+    if(req.url === '/home'){
+        res.write('Welcome home')
+    }
+    else if(req.url === '/About'){
+        res.write('Welcome to About Us page')
+    }
+    else if(req.url === '/node'){
+        res.write('Welcome to my Node Js project')
+    }
+
+    res.end()
 })
 
 server.listen(3000)
